4668f3ed2782fe4f0b68a2a8babe3fbb06389958
Vendel Toreki <vendel.toreki@liferay.com>
2019-05-06 15:11:15 +0200

    LPS-95113 Check before deletion whether the Layout has already been deleted as a child of another Layout

--- modules/apps/layout/layout-impl/src/main/java/com/liferay/layout/internal/exportimport/data/handler/StagedLayoutSetStagedModelDataHandler.java
+++ modules/apps/layout/layout-impl/src/main/java/com/liferay/layout/internal/exportimport/data/handler/StagedLayoutSetStagedModelDataHandler.java
@@ -138,7 +138,11 @@ public class StagedLayoutSetStagedModelDataHandler
 				layout.getSourcePrototypeLayoutUuid(),
 				layoutSetPrototype.getGroupId(), true);
 
-			if (sourcePrototypeLayout == null) {
+			if ((sourcePrototypeLayout == null) &&
+				_layoutLocalService.hasLayout(
+					layout.getUuid(), layout.getGroupId(),
+					layout.isPrivateLayout())) {
+
 				_layoutLocalService.deleteLayout(
 					layout, false,
 					ServiceContextThreadLocal.getServiceContext());
