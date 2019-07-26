5739265301bfbfb8cc116212ec6361aec4df3acb
Minhchau <minhchau.dang@liferay.com>
2019-07-26 15:11:31 -0700

    PTR-1065 Log the mvccVersions being merged in each thread

--- portal-impl/src/com/liferay/portlet/sites/util/SitesImpl.java
+++ portal-impl/src/com/liferay/portlet/sites/util/SitesImpl.java
@@ -1348,6 +1348,18 @@ public class SitesImpl implements Sites {
 		try {
 			MergeLayoutPrototypesThreadLocal.setInProgress(true);
 
+			if (_log.isDebugEnabled()) {
+				if (_log.isDebugEnabled()) {
+					_log.debug(StringBundler.concat(
+						"Applying layout set prototype ",
+						layoutSetPrototype.getUuid(),
+						" (mvccVersion ", layoutSetPrototype.getMvccVersion(),
+						"_ to layout set ", layoutSet.getLayoutSetId(),
+						" (mvccVersion ", layoutSet.getMvccVersion(), ")"));
+				}
+			}
+
+
 			boolean importData = true;
 
 			long lastMergeTime = GetterUtil.getLong(
@@ -1732,6 +1744,14 @@ public class SitesImpl implements Sites {
 
 		try {
 			MergeLayoutPrototypesThreadLocal.setInProgress(true);
+
+			if (_log.isDebugEnabled()) {
+				_log.debug(StringBundler.concat(
+					"Applying layout prototype ", layoutPrototype.getUuid(),
+					" (mvccVersion ", layoutPrototype.getMvccVersion(),
+					"_ to layout ", layout.getPlid(), " (mvccVersion ",
+					layout.getMvccVersion(), ")"));
+			}
 
 			applyLayoutPrototype(layoutPrototype, layout, true);
 		}
