a37e298db0b8f5d9c7eb119365288bbb74a9602f
Kimberly Chau <kimberly.chau@liferay.com>
2020-01-17 09:47:30 -0800

    LPS-94690 Set live URL, depending if they checked the checkbox

--- modules/apps/export-import/export-import-service/src/main/java/com/liferay/exportimport/internal/staging/StagingImpl.java
+++ modules/apps/export-import/export-import-service/src/main/java/com/liferay/exportimport/internal/staging/StagingImpl.java
@@ -1856,6 +1856,14 @@ public class StagingImpl implements Staging {
 		UnicodeProperties typeSettingsProperties =
 			stagingGroup.getTypeSettingsProperties();
 
+		boolean setRemoteSiteURL = GetterUtil.getBoolean(
+			typeSettingsProperties.getProperty("setRemoteSiteURL"));
+
+		if (setRemoteSiteURL) {
+			return GetterUtil.getString(
+				typeSettingsProperties.getProperty("remoteSiteURL"));
+		}
+
 		HttpPrincipal httpPrincipal = new HttpPrincipal(
 			_stagingURLHelper.buildRemoteURL(typeSettingsProperties),
 			user.getLogin(), user.getPassword(), user.isPasswordEncrypted());
