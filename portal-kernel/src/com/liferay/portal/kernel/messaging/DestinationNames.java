3f69b1fad22dc546176f16b0b96073f8a0605275
Vendel Toreki <vendel.toreki@liferay.com>
2023-09-25 11:54:37 +0200

    LPS-195445 Revert "LPS-192680 Remove constant"
    This reverts commit 61c3fb04f53603620a9205bc56ff76cdf4da55a3.

--- portal-kernel/src/com/liferay/portal/kernel/messaging/DestinationNames.java
+++ portal-kernel/src/com/liferay/portal/kernel/messaging/DestinationNames.java
@@ -60,6 +60,8 @@ public interface DestinationNames {
 
 	public static final String FLAGS = "liferay/flags";
 
+	public static final String HOT_DEPLOY = "liferay/hot_deploy";
+
 	public static final String LAYOUTS_LOCAL_PUBLISHER =
 		"liferay/layouts_local_publisher";
 
