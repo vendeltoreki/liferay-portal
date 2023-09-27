25529f956aaab40747b5be15bdcf748f5ccad89f
Vendel Toreki <vendel.toreki@liferay.com>
2023-09-27 13:24:07 +0200

    LPS-195445 Revert "LPS-192680 Remove MessagingHotDeployListener"
    This reverts commit 9b2788a6418db4da7230f555e083b15f5572ca45.

---
+++ portal-impl/src/com/liferay/portal/deploy/hot/MessagingHotDeployListener.java
@@ -0,0 +1,77 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.portal.deploy.hot;
+
+import com.liferay.portal.kernel.deploy.hot.BaseHotDeployListener;
+import com.liferay.portal.kernel.deploy.hot.HotDeployEvent;
+import com.liferay.portal.kernel.deploy.hot.HotDeployException;
+import com.liferay.portal.kernel.messaging.DestinationNames;
+import com.liferay.portal.kernel.messaging.Message;
+import com.liferay.portal.kernel.messaging.MessageBusUtil;
+
+import javax.servlet.ServletContext;
+
+/**
+ * @author Brian Wing Shun Chan
+ */
+public class MessagingHotDeployListener extends BaseHotDeployListener {
+
+	@Override
+	public void invokeDeploy(HotDeployEvent hotDeployEvent)
+		throws HotDeployException {
+
+		try {
+			doInvokeDeploy(hotDeployEvent);
+		}
+		catch (Throwable throwable) {
+			throwHotDeployException(
+				hotDeployEvent, "Error sending deploy message for ", throwable);
+		}
+	}
+
+	@Override
+	public void invokeUndeploy(HotDeployEvent hotDeployEvent)
+		throws HotDeployException {
+
+		try {
+			doInvokeUndeploy(hotDeployEvent);
+		}
+		catch (Throwable throwable) {
+			throwHotDeployException(
+				hotDeployEvent, "Error sending undeploy message for ",
+				throwable);
+		}
+	}
+
+	protected void doInvokeDeploy(HotDeployEvent hotDeployEvent)
+		throws Exception {
+
+		ServletContext servletContext = hotDeployEvent.getServletContext();
+
+		Message message = new Message();
+
+		message.put("command", "deploy");
+		message.put(
+			"servletContextName", servletContext.getServletContextName());
+
+		MessageBusUtil.sendMessage(DestinationNames.HOT_DEPLOY, message);
+	}
+
+	protected void doInvokeUndeploy(HotDeployEvent hotDeployEvent)
+		throws Exception {
+
+		ServletContext servletContext = hotDeployEvent.getServletContext();
+
+		Message message = new Message();
+
+		message.put("command", "undeploy");
+		message.put(
+			"servletContextName", servletContext.getServletContextName());
+
+		MessageBusUtil.sendMessage(DestinationNames.HOT_DEPLOY, message);
+	}
+
+}
