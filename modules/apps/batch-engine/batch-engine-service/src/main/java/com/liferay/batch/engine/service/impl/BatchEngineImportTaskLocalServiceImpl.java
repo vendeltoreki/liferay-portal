0be85f6ce9a06b5d080e8fec3c9dd4ac34929cb5
Vendel Töreki <vendel.toreki@liferay.com>
2025-10-16 12:46:59 +0200

    LPD-66580 Move the actual batch import execution method to the LocalService layer

--- modules/apps/batch-engine/batch-engine-service/src/main/java/com/liferay/batch/engine/service/impl/BatchEngineImportTaskLocalServiceImpl.java
+++ modules/apps/batch-engine/batch-engine-service/src/main/java/com/liferay/batch/engine/service/impl/BatchEngineImportTaskLocalServiceImpl.java
@@ -5,14 +5,24 @@
 
 package com.liferay.batch.engine.service.impl;
 
+import com.liferay.batch.engine.BatchEngineImportTaskExecutor;
+import com.liferay.batch.engine.BatchEngineTaskContentType;
+import com.liferay.batch.engine.BatchEngineTaskExecuteStatus;
 import com.liferay.batch.engine.BatchEngineTaskItemDelegate;
 import com.liferay.batch.engine.BatchEngineTaskItemDelegateRegistry;
+import com.liferay.batch.engine.BatchEngineTaskOperation;
+import com.liferay.batch.engine.ItemClassRegistry;
+import com.liferay.batch.engine.configuration.BatchEngineTaskCompanyConfiguration;
+import com.liferay.batch.engine.constants.BatchEngineImportTaskConstants;
+import com.liferay.batch.engine.constants.CreateStrategy;
 import com.liferay.batch.engine.exception.BatchEngineImportTaskParametersException;
 import com.liferay.batch.engine.model.BatchEngineImportTask;
 import com.liferay.batch.engine.service.base.BatchEngineImportTaskLocalServiceBaseImpl;
 import com.liferay.batch.engine.service.persistence.BatchEngineImportTaskErrorPersistence;
+import com.liferay.petra.executor.PortalExecutorManager;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.aop.AopService;
+import com.liferay.portal.configuration.module.configuration.ConfigurationProvider;
 import com.liferay.portal.kernel.change.tracking.CTAware;
 import com.liferay.portal.kernel.dao.jdbc.OutputBlob;
 import com.liferay.portal.kernel.exception.PortalException;
@@ -20,12 +30,18 @@ import com.liferay.portal.kernel.io.unsync.UnsyncByteArrayInputStream;
 import com.liferay.portal.kernel.transaction.Propagation;
 import com.liferay.portal.kernel.transaction.Transactional;
 import com.liferay.portal.kernel.util.OrderByComparator;
+import com.liferay.portal.kernel.util.SetUtil;
+import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.Validator;
 
 import java.io.Serializable;
 
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ExecutorService;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -127,6 +143,66 @@ public class BatchEngineImportTaskLocalServiceImpl
 	}
 
 	@Override
+	public BatchEngineImportTask executeBatchEngineImportTask(
+			BatchEngineTaskOperation batchEngineTaskOperation, long companyId,
+			String batchExternalReferenceCode, byte[] bytes, String callbackURL,
+			String className, String createStrategy,
+			BatchEngineTaskContentType batchEngineTaskContentType,
+			String externalReferenceCode, String fieldNameMappingString,
+			String importStrategy, Map<String, Serializable> parameters,
+			String taskItemDelegateName, String updateStrategy, long userId)
+		throws Exception {
+
+		Class<?> clazz = _itemClassRegistry.getItemClass(className);
+
+		if (clazz == null) {
+			throw new IllegalArgumentException(
+				"Unknown class name: " + className);
+		}
+
+		if (!_hasUniqueScopeParameters(parameters)) {
+			throw new IllegalArgumentException(
+				"Unsupported combination of scope parameters");
+		}
+
+		if (Validator.isNotNull(batchExternalReferenceCode)) {
+			parameters.put("externalReferenceCode", batchExternalReferenceCode);
+		}
+
+		if (createStrategy != null) {
+			CreateStrategy createStrategyEnum = CreateStrategy.valueOf(
+				createStrategy);
+
+			parameters.put(
+				"createStrategy", createStrategyEnum.getDBOperation());
+		}
+
+		if (updateStrategy != null) {
+			parameters.put("updateStrategy", updateStrategy);
+		}
+
+		BatchEngineImportTask batchEngineImportTask = addBatchEngineImportTask(
+			externalReferenceCode, companyId, userId,
+			_itemClassBatchSizeMap.getOrDefault(
+				className, _getImportBatchSize(companyId)),
+			callbackURL, className, bytes,
+			StringUtil.upperCase(batchEngineTaskContentType.toString()),
+			BatchEngineTaskExecuteStatus.INITIAL.name(),
+			_toMap(fieldNameMappingString), _toImportStrategy(importStrategy),
+			batchEngineTaskOperation.name(), parameters, taskItemDelegateName);
+
+		ExecutorService executorService =
+			_portalExecutorManager.getPortalExecutor(
+				BatchEngineImportTaskLocalServiceImpl.class.getName());
+
+		executorService.submit(
+			() -> _batchEngineImportTaskExecutor.execute(
+				batchEngineImportTask));
+
+		return batchEngineImportTask;
+	}
+
+	@Override
 	public List<BatchEngineImportTask> getBatchEngineImportTasks(
 		long companyId, int start, int end) {
 
@@ -154,6 +230,73 @@ public class BatchEngineImportTaskLocalServiceImpl
 	@Override
 	public int getBatchEngineImportTasksCount(long companyId) {
 		return batchEngineImportTaskPersistence.countByCompanyId(companyId);
+	}
+
+	private int _getImportBatchSize(long companyId) throws Exception {
+		BatchEngineTaskCompanyConfiguration
+			batchEngineTaskCompanyConfiguration =
+				_configurationProvider.getCompanyConfiguration(
+					BatchEngineTaskCompanyConfiguration.class, companyId);
+
+		return batchEngineTaskCompanyConfiguration.importBatchSize();
+	}
+
+	private boolean _hasUniqueScopeParameters(
+		Map<String, Serializable> parameters) {
+
+		Set<String> assetLibraryScopeKeys = SetUtil.fromArray(
+			"assetLibraryExternalReferenceCode", "assetLibraryId");
+		Set<String> siteScopeKeys = SetUtil.fromArray(
+			"siteExternalReferenceCode", "siteId");
+
+		boolean hasAssetLibraryScopeKey = false;
+		boolean hasSiteScopeKey = false;
+
+		for (String key : parameters.keySet()) {
+			if (assetLibraryScopeKeys.contains(key)) {
+				hasAssetLibraryScopeKey = true;
+			}
+			else if (siteScopeKeys.contains(key)) {
+				hasSiteScopeKey = true;
+			}
+
+			if (hasAssetLibraryScopeKey && hasSiteScopeKey) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	private int _toImportStrategy(String importStrategy) {
+		if ((importStrategy == null) ||
+			importStrategy.equals(
+				BatchEngineImportTaskConstants.
+					IMPORT_STRATEGY_STRING_ON_ERROR_FAIL)) {
+
+			return BatchEngineImportTaskConstants.IMPORT_STRATEGY_ON_ERROR_FAIL;
+		}
+
+		return BatchEngineImportTaskConstants.IMPORT_STRATEGY_ON_ERROR_CONTINUE;
+	}
+
+	private Map<String, String> _toMap(String fieldNameMappingString) {
+		if (Validator.isNull(fieldNameMappingString)) {
+			return Collections.emptyMap();
+		}
+
+		Map<String, String> fieldNameMappingMap = new HashMap<>();
+
+		String[] fieldNameMappings = StringUtil.split(
+			fieldNameMappingString, ',');
+
+		for (String fieldNameMapping : fieldNameMappings) {
+			String[] fieldNames = StringUtil.split(fieldNameMapping, '=');
+
+			fieldNameMappingMap.put(fieldNames[0], fieldNames[1]);
+		}
+
+		return fieldNameMappingMap;
 	}
 
 	private void _validateDelimiter(String delimiter)
@@ -210,7 +353,21 @@ public class BatchEngineImportTaskLocalServiceImpl
 		_batchEngineImportTaskErrorPersistence;
 
 	@Reference
+	private BatchEngineImportTaskExecutor _batchEngineImportTaskExecutor;
+
+	@Reference
 	private BatchEngineTaskItemDelegateRegistry
 		_batchEngineTaskItemDelegateRegistry;
+
+	@Reference
+	private ConfigurationProvider _configurationProvider;
+
+	private final Map<String, Integer> _itemClassBatchSizeMap = new HashMap<>();
+
+	@Reference
+	private ItemClassRegistry _itemClassRegistry;
+
+	@Reference
+	private PortalExecutorManager _portalExecutorManager;
 
 }
