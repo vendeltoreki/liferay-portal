18b878feda62c51b47af27bc1a0783f2cff6b3f9
Alberto Moreno <alberto.moreno@liferay.com>
2026-01-15 13:09:13 +0100

    LPD-76007 Allow also permissions actionId since the checkPermissions from API requests should check that actionId

--- modules/apps/export-import/export-import-service/src/main/java/com/liferay/exportimport/internal/staging/permission/StagingPermissionImpl.java
+++ modules/apps/export-import/export-import-service/src/main/java/com/liferay/exportimport/internal/staging/permission/StagingPermissionImpl.java
@@ -73,6 +73,7 @@ public class StagingPermissionImpl implements StagingPermission {
 			!actionId.equals(ActionKeys.DELETE) &&
 			!actionId.equals(ActionKeys.DELETE_DISCUSSION) &&
 			!actionId.equals(ActionKeys.DOWNLOAD) &&
+			!actionId.equals(ActionKeys.PERMISSIONS) &&
 			!actionId.equals(ActionKeys.UPDATE_DISCUSSION) &&
 			!actionId.equals(ActionKeys.VIEW) &&
 			group.hasLocalOrRemoteStagingGroup() &&
