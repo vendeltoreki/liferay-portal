e6e061c231e6faa759858bd2906c542f9c6ef8ad
Vendel Töreki <vendel.toreki@liferay.com>
2026-06-01 18:38:30 +0200

    LPD-88511 Add tests for the shared language key resolver

--- modules/apps/batch-engine/batch-engine-service/src/test/java/com/liferay/batch/engine/internal/action/LanguageKeyImportTaskPreActionTest.java
+++ modules/apps/batch-engine/batch-engine-service/src/test/java/com/liferay/batch/engine/internal/action/LanguageKeyImportTaskPreActionTest.java
@@ -5,22 +5,15 @@
 
 package com.liferay.batch.engine.internal.action;
 
-import com.liferay.batch.engine.model.BatchEngineImportTask;
-import com.liferay.portal.kernel.language.Language;
+import com.liferay.batch.engine.language.LanguageKeyResolver;
 import com.liferay.portal.kernel.util.HashMapBuilder;
-import com.liferay.portal.kernel.util.LinkedHashMapBuilder;
-import com.liferay.portal.kernel.util.LocaleUtil;
-import com.liferay.portal.kernel.util.ProxyUtil;
 import com.liferay.portal.test.rule.LiferayUnitTestRule;
 
 import java.lang.reflect.Field;
-import java.lang.reflect.InvocationHandler;
-import java.lang.reflect.Method;
 
-import java.util.LinkedHashSet;
+import java.util.LinkedHashMap;
 import java.util.Locale;
 import java.util.Map;
-import java.util.Set;
 
 import org.junit.Assert;
 import org.junit.Before;
@@ -43,71 +36,47 @@ public class LanguageKeyImportTaskPreActionTest {
 		_languageKeyImportTaskPreAction = new LanguageKeyImportTaskPreAction();
 
 		Field field = LanguageKeyImportTaskPreAction.class.getDeclaredField(
-			"_language");
+			"_languageKeyResolver");
 
 		field.setAccessible(true);
 
-		field.set(_languageKeyImportTaskPreAction, _createLanguage());
+		field.set(
+			_languageKeyImportTaskPreAction, new TestLanguageKeyResolver());
 	}
 
 	@Test
-	public void testEmbeddedPlaceholderResolvedInline() throws Exception {
+	public void testI18nFieldResolved() throws Exception {
 
-		// AC4
+		// AC1, AC8
 
-		Assert.assertEquals(
-			"prefix Welcome suffix",
-			_resolve(
-				"name_i18n", "en_US",
-				"prefix $LANG_KEY[welcome][en_US] suffix"));
-	}
+		TestItem testItem = new TestItem();
 
-	@Test
-	public void testMalformedEmptyKeyLeftUnchanged() throws Exception {
-
-		// AC5
-
-		String value = "$LANG_KEY[][en_US]";
-
-		Assert.assertEquals(value, _resolve("name_i18n", "en_US", value));
-	}
-
-	@Test
-	public void testMalformedLocaleFormatLeftUnchanged() throws Exception {
-
-		// AC5
-
-		String value = "$LANG_KEY[welcome][en-US]";
-
-		Assert.assertEquals(value, _resolve("name_i18n", "en_US", value));
-	}
-
-	@Test
-	public void testMalformedMissingLocaleBracketLeftUnchanged()
-		throws Exception {
-
-		// AC5
-
-		String value = "$LANG_KEY[welcome]";
-
-		Assert.assertEquals(value, _resolve("name_i18n", "en_US", value));
-	}
-
-	@Test
-	public void testMultipleLocalesResolvedIndependently() throws Exception {
-
-		// AC1
-
-		Map<String, String> resolvedI18nMap = _run(
-			"name_i18n",
-			LinkedHashMapBuilder.put(
+		testItem.setName_i18n(
+			HashMapBuilder.put(
 				"en_US", "$LANG_KEY[welcome][en_US]"
-			).put(
-				"es_ES", "$LANG_KEY[welcome][es_ES]"
 			).build());
 
-		Assert.assertEquals("Welcome", resolvedI18nMap.get("en_US"));
-		Assert.assertEquals("Bienvenido", resolvedI18nMap.get("es_ES"));
+		_languageKeyImportTaskPreAction.run(null, null, null, testItem);
+
+		Map<String, String> name_i18n = testItem.getName_i18n();
+
+		Assert.assertEquals("Welcome", name_i18n.get("en_US"));
+	}
+
+	@Test
+	public void testI18nValueWithoutPlaceholderUnchanged() throws Exception {
+		TestItem testItem = new TestItem();
+
+		testItem.setName_i18n(
+			HashMapBuilder.put(
+				"en_US", "Hello"
+			).build());
+
+		_languageKeyImportTaskPreAction.run(null, null, null, testItem);
+
+		Map<String, String> name_i18n = testItem.getName_i18n();
+
+		Assert.assertEquals("Hello", name_i18n.get("en_US"));
 	}
 
 	@Test
@@ -119,49 +88,10 @@ public class LanguageKeyImportTaskPreActionTest {
 
 		testItem.setDescription("$LANG_KEY[welcome][en_US]");
 
-		_languageKeyImportTaskPreAction.run(
-			_createBatchEngineImportTask(), null, null, testItem);
+		_languageKeyImportTaskPreAction.run(null, null, null, testItem);
 
 		Assert.assertEquals(
 			"$LANG_KEY[welcome][en_US]", testItem.getDescription());
-	}
-
-	@Test
-	public void testUnknownKeyLeftEmpty() throws Exception {
-
-		// AC2
-
-		Assert.assertEquals(
-			"", _resolve("name_i18n", "en_US", "$LANG_KEY[missing][en_US]"));
-	}
-
-	@Test
-	public void testUnknownLocaleCaseMismatchLeftEmpty() throws Exception {
-
-		// AC2, AC6
-
-		Assert.assertEquals(
-			"", _resolve("name_i18n", "en_US", "$LANG_KEY[welcome][en_us]"));
-	}
-
-	@Test
-	public void testValidPlaceholderResolved() throws Exception {
-
-		// AC1
-
-		Assert.assertEquals(
-			"Welcome",
-			_resolve("name_i18n", "en_US", "$LANG_KEY[welcome][en_US]"));
-	}
-
-	@Test
-	public void testWhitespaceInBracketsLeftUnchanged() throws Exception {
-
-		// AC7
-
-		String value = "$LANG_KEY[ welcome ][ en_US ]";
-
-		Assert.assertEquals(value, _resolve("name_i18n", "en_US", value));
 	}
 
 	public static class TestItem {
@@ -187,113 +117,32 @@ public class LanguageKeyImportTaskPreActionTest {
 
 	}
 
-	private BatchEngineImportTask _createBatchEngineImportTask() {
-		return (BatchEngineImportTask)ProxyUtil.newProxyInstance(
-			BatchEngineImportTask.class.getClassLoader(),
-			new Class<?>[] {BatchEngineImportTask.class},
-			new InvocationHandler() {
+	private LanguageKeyImportTaskPreAction _languageKeyImportTaskPreAction;
 
-				@Override
-				public Object invoke(
-					Object proxy, Method method, Object[] args) {
+	private static class TestLanguageKeyResolver
+		implements LanguageKeyResolver {
 
-					String name = method.getName();
+		@Override
+		public Map<Locale, String> resolve(Map<Locale, String> localizedMap) {
+			Map<Locale, String> resolvedLocalizedMap = new LinkedHashMap<>();
 
-					if (name.equals("getCompanyId")) {
-						return _COMPANY_ID;
-					}
+			for (Map.Entry<Locale, String> entry : localizedMap.entrySet()) {
+				resolvedLocalizedMap.put(
+					entry.getKey(), resolve(entry.getValue()));
+			}
 
-					if (name.equals("getBatchEngineImportTaskId")) {
-						return 1L;
-					}
-
-					return null;
-				}
-
-			});
-	}
-
-	private Language _createLanguage() {
-		Map<String, String> translations = HashMapBuilder.put(
-			"en_US/welcome", "Welcome"
-		).put(
-			"es_ES/welcome", "Bienvenido"
-		).build();
-
-		Set<Locale> availableLocales = new LinkedHashSet<>();
-
-		availableLocales.add(LocaleUtil.US);
-		availableLocales.add(LocaleUtil.SPAIN);
-
-		return (Language)ProxyUtil.newProxyInstance(
-			Language.class.getClassLoader(), new Class<?>[] {Language.class},
-			new InvocationHandler() {
-
-				@Override
-				public Object invoke(
-					Object proxy, Method method, Object[] args) {
-
-					String name = method.getName();
-
-					if (name.equals("getAvailableLocales")) {
-						return availableLocales;
-					}
-
-					if (name.equals("get") && (args.length == 2) &&
-						(args[0] instanceof Locale)) {
-
-						String key = (String)args[1];
-
-						String translation = translations.get(
-							LocaleUtil.toLanguageId((Locale)args[0]) + "/" +
-								key);
-
-						// Language returns the key itself when no translation
-						// exists.
-
-						if (translation == null) {
-							return key;
-						}
-
-						return translation;
-					}
-
-					return null;
-				}
-
-			});
-	}
-
-	private String _resolve(String fieldName, String locale, String value)
-		throws Exception {
-
-		Map<String, String> resolvedI18nMap = _run(
-			fieldName,
-			LinkedHashMapBuilder.put(
-				locale, value
-			).build());
-
-		return resolvedI18nMap.get(locale);
-	}
-
-	private Map<String, String> _run(
-			String fieldName, Map<String, String> i18nMap)
-		throws Exception {
-
-		TestItem testItem = new TestItem();
-
-		if (fieldName.equals("name_i18n")) {
-			testItem.setName_i18n(i18nMap);
+			return resolvedLocalizedMap;
 		}
 
-		_languageKeyImportTaskPreAction.run(
-			_createBatchEngineImportTask(), null, null, testItem);
+		@Override
+		public String resolve(String value) {
+			if (value.equals("$LANG_KEY[welcome][en_US]")) {
+				return "Welcome";
+			}
 
-		return testItem.getName_i18n();
+			return value;
+		}
+
 	}
-
-	private static final long _COMPANY_ID = 1L;
-
-	private LanguageKeyImportTaskPreAction _languageKeyImportTaskPreAction;
 
 }
---
+++ modules/apps/batch-engine/batch-engine-service/src/test/java/com/liferay/batch/engine/internal/language/LanguageKeyResolverImplTest.java
@@ -0,0 +1,202 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.batch.engine.internal.language;
+
+import com.liferay.portal.kernel.language.Language;
+import com.liferay.portal.kernel.util.HashMapBuilder;
+import com.liferay.portal.kernel.util.LinkedHashMapBuilder;
+import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.ProxyUtil;
+import com.liferay.portal.test.rule.LiferayUnitTestRule;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+
+import java.util.LinkedHashSet;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Set;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+
+/**
+ * @author Vendel Toreki
+ */
+public class LanguageKeyResolverImplTest {
+
+	@ClassRule
+	@Rule
+	public static final LiferayUnitTestRule liferayUnitTestRule =
+		LiferayUnitTestRule.INSTANCE;
+
+	@Before
+	public void setUp() throws Exception {
+		_languageKeyResolverImpl = new LanguageKeyResolverImpl();
+
+		Field field = LanguageKeyResolverImpl.class.getDeclaredField(
+			"_language");
+
+		field.setAccessible(true);
+
+		field.set(_languageKeyResolverImpl, _createLanguage());
+	}
+
+	@Test
+	public void testEmbeddedPlaceholderResolvedInline() {
+
+		// AC4
+
+		Assert.assertEquals(
+			"prefix Welcome suffix",
+			_languageKeyResolverImpl.resolve(
+				"prefix $LANG_KEY[welcome][en_US] suffix"));
+	}
+
+	@Test
+	public void testLocalizedMapResolvedPerValue() {
+
+		// AC1
+
+		Map<Locale, String> resolvedLocalizedMap =
+			_languageKeyResolverImpl.resolve(
+				LinkedHashMapBuilder.put(
+					LocaleUtil.US, "$LANG_KEY[welcome][en_US]"
+				).put(
+					LocaleUtil.SPAIN, "$LANG_KEY[welcome][es_ES]"
+				).build());
+
+		Assert.assertEquals("Welcome", resolvedLocalizedMap.get(LocaleUtil.US));
+		Assert.assertEquals(
+			"Bienvenido", resolvedLocalizedMap.get(LocaleUtil.SPAIN));
+	}
+
+	@Test
+	public void testMalformedEmptyKeyLeftUnchanged() {
+
+		// AC5
+
+		Assert.assertEquals(
+			"$LANG_KEY[][en_US]",
+			_languageKeyResolverImpl.resolve("$LANG_KEY[][en_US]"));
+	}
+
+	@Test
+	public void testMalformedLocaleFormatLeftUnchanged() {
+
+		// AC5
+
+		Assert.assertEquals(
+			"$LANG_KEY[welcome][en-US]",
+			_languageKeyResolverImpl.resolve("$LANG_KEY[welcome][en-US]"));
+	}
+
+	@Test
+	public void testMalformedMissingLocaleBracketLeftUnchanged() {
+
+		// AC5
+
+		Assert.assertEquals(
+			"$LANG_KEY[welcome]",
+			_languageKeyResolverImpl.resolve("$LANG_KEY[welcome]"));
+	}
+
+	@Test
+	public void testUnknownKeyLeftEmpty() {
+
+		// AC2
+
+		Assert.assertEquals(
+			"", _languageKeyResolverImpl.resolve("$LANG_KEY[missing][en_US]"));
+	}
+
+	@Test
+	public void testUnknownLocaleCaseMismatchLeftEmpty() {
+
+		// AC2, AC6
+
+		Assert.assertEquals(
+			"", _languageKeyResolverImpl.resolve("$LANG_KEY[welcome][en_us]"));
+	}
+
+	@Test
+	public void testValidPlaceholderResolved() {
+
+		// AC1
+
+		Assert.assertEquals(
+			"Welcome",
+			_languageKeyResolverImpl.resolve("$LANG_KEY[welcome][en_US]"));
+	}
+
+	@Test
+	public void testWhitespaceInBracketsLeftUnchanged() {
+
+		// AC7
+
+		Assert.assertEquals(
+			"$LANG_KEY[ welcome ][ en_US ]",
+			_languageKeyResolverImpl.resolve("$LANG_KEY[ welcome ][ en_US ]"));
+	}
+
+	private Language _createLanguage() {
+		Map<String, String> translations = HashMapBuilder.put(
+			"en_US/welcome", "Welcome"
+		).put(
+			"es_ES/welcome", "Bienvenido"
+		).build();
+
+		Set<Locale> availableLocales = new LinkedHashSet<>();
+
+		availableLocales.add(LocaleUtil.US);
+		availableLocales.add(LocaleUtil.SPAIN);
+
+		return (Language)ProxyUtil.newProxyInstance(
+			Language.class.getClassLoader(), new Class<?>[] {Language.class},
+			new InvocationHandler() {
+
+				@Override
+				public Object invoke(
+					Object proxy, Method method, Object[] args) {
+
+					String name = method.getName();
+
+					if (name.equals("getAvailableLocales") && (args == null)) {
+						return availableLocales;
+					}
+
+					if (name.equals("get") && (args.length == 2) &&
+						(args[0] instanceof Locale)) {
+
+						String key = (String)args[1];
+
+						String translation = translations.get(
+							LocaleUtil.toLanguageId((Locale)args[0]) + "/" +
+								key);
+
+						// Language returns the key itself when no translation
+						// exists.
+
+						if (translation == null) {
+							return key;
+						}
+
+						return translation;
+					}
+
+					return null;
+				}
+
+			});
+	}
+
+	private LanguageKeyResolverImpl _languageKeyResolverImpl;
+
+}
